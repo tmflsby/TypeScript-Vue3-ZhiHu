27614b4d3705f05aabb0d7abe34788c452d50818
tmflsby <823246840@qq.com>
2021-02-05 17:14:47 +0800

    add ColumnList,GlobalHeader,Dropdown,DropdownItem components

--- zhihu/src/App.vue
+++ zhihu/src/App.vue
@@ -1,27 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <GlobalHeader :user="currentUser"/>
+    <ColumnList :list="list"/>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
+const columnData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1专栏',
+    description: '12312132131232132133333333',
+    avatar: 'https://avatars.githubusercontent.com/u/48079487?s=60&v=4'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '12312132131232132133333333'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '12312132131232132133333333',
+    avatar: 'https://avatars.githubusercontent.com/u/48079487?s=60&v=4'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '12312132131232132133333333'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '12312132131232132133333333',
+    avatar: 'https://avatars.githubusercontent.com/u/48079487?s=60&v=4'
+  }
+]
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'curry'
+}
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: { ColumnList, GlobalHeader },
+  setup () {
+    return {
+      list: columnData,
+      currentUser
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ zhihu/src/components/GlobalHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-dark bg-primary justify-content-between mb-4 px-4">
+    <a class="navbar-brand" href="/">知乎专栏</a>
+    <ul class="list-inline mb-0" v-if="!user.isLogin">
+      <li class="list-inline-item">
+        <a class="btn btn-outline-light my-2" href="/">登录</a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn btn-outline-light my-2" href="/">注册</a>
+      </li>
+    </ul>
+    <ul class="list-inline mb-0" v-else>
+      <li class="list-inline-item">
+        <Dropdown :title="`你好 ${user.name}`">
+          <DropdownItem><a class="dropdown-item" href="/">新建文章</a></DropdownItem>
+          <DropdownItem disabled><a class="dropdown-item" href="/">编辑资料</a></DropdownItem>
+          <DropdownItem><a class="dropdown-item" href="/">退出登录</a></DropdownItem>
+        </Dropdown>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Dropdown from '@/components/Dropdown.vue'
+import DropdownItem from '@/components/DropdownItem.vue'
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: number;
+}
+export default defineComponent({
+  name: 'GlobalHeader',
+  components: { Dropdown, DropdownItem },
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
